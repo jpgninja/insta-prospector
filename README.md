5d3e089bcc46b55427569b1d05af94679796fca3
Chris Mewhort <chrismewhort@gmail.com>
2017-08-18 12:18:27 -0400

    working alpha

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# insta-prospector
+# Instagram Prospector
+
+Useful for businesses looking to prospect on Instagram.
+
+## Usage
+
+* Login to [Instagram](https://www.instagram.com)
+* Inject `prospector.js` into the page – _I'm leaving this up to you, but eventually this would turn into an extension._
+* Search for a hashtag (ie. [#photography](https://www.instagram.com/explore/tags/photography/))
+* Click _"Load more"_
+* Scroll down, and get a decent number of photos on screen
+* Click `Scrape` button
+* Wait...
+* ???
+* Profit!
+
+## Thoughts
+
+* Would love help/suggestions
+* Things will move around substantially in the next few months
--- prospector.js
+++ prospector.js
@@ -1,27 +1,45 @@
-// Here You can type your custom JavaScript...
-jQuery(function() {
-  let profile = {};
+/**
+ *
+ *
+ */
+jQuery(() => {
+  // let profile = {};
 
-  let getTitle = () => {
-    return jQuery('section main article header h1').text();
-  }
-  let is_profile = function() {
-      let test = getTitle();
-      return (test.length > 0);
-  }
-  let is_photo = function() {
-      let test = getUsername();
-      return (test.length > 0);
-  }
-  let getUsername = function( ) {
-      return jQuery('article header div div div a').text();
+
+  /**
+   * init
+   *
+   * Bootstrap our scraper plugin
+   */
+  let init = () => {
+    let cachebuster = new Date();
+    cachebuster = cachebuster.getTime();
+
+    // Inject styles
+    jQuery('<link href="//loc.insta.loc/prospector.css?'+ cachebuster +'" rel="stylesheet"></link>').appendTo('head');
+
+    // Inject dom elements
+    jQuery('<div href="#" id="ng_prospector"><a href="#" class="btn scrape">Scrape <span class="count"></span></a></div>').appendTo('body');
+
+    // Inject scripts
+    // jQuery('<script src="/loc.insta.loc/lib/clipboard.min.js"></script>').appendTo('body');
+
+    // Add Event Listeners
+    addListeners();
   }
 
+
+  /**
+   * addListeners
+   *
+   * Add various needed Event Listeners
+   */
   let addListeners = () => {
-      jQuery('#ng_prospector .scrape').on('click', scrapePage);
-      // jQuery('body main article > div:nth(1) > a').on('click', () => { console.log('clicked!'); });
-      // jQuery(document).on('scroll', scrollHandler);
+    jQuery('#ng_prospector .scrape').on('click', scrapePage);
+    // jQuery('body main article > div:nth(1) > a').on('click', () => { console.log('clicked!'); });
+    // jQuery(document).on('scroll', scrollHandler);
   }
+
 
   /**
    * scrapePage
@@ -43,21 +61,32 @@ jQuery(function() {
       return scrapeProfile();
     }
   }
+
+
+  /**
+   * scrapeHashtag
+   *
+   * @param unformatted Array should be empty initially
+   * @param formatted Array should be empty initially
+   */
   let scrapeHashtag = (unformatted = [], formatted = []) => {
-    unformatted = jQuery('._cmdpi ._70iju');
+    unformatted = jQuery('._cmdpi ._70iju div > a');
 
     unformatted.each((i, photo) => {
-      // First 9 are ads
-      if (i>8) {
-        // Instead, we want the most recent
-        let photoUrl = jQuery(photo).find('div > a').attr('href').split("?tagged=")[0];
+      if (i>8) { // Skip the first 9 (ads), we just want the "most recent"
+        let photoUrl = jQuery(photo).attr('href').split("?tagged=")[0];
         formatted.push( photoUrl );
-        // console.log(i, photoUrl );
       }
     });
 
-    // Scrape this list of photo urlsa
-    scrapeUsernamesFromPhotoUrls(formatted);
+    // Scrape this list of photo urls
+    if (formatted.length) {
+      scrapeUsernamesFromPhotoUrls(formatted);
+    }
+    else {
+      console.error("Ack! Didn't find any photos");
+      // console.log("%s unformatted photo items%s", (unformatted.length) ? "Found "+unformatted.length : "Didn't find any", (unformatted.length) ? '' : ', either' );
+    }
   }
 
 
@@ -68,33 +97,109 @@ jQuery(function() {
    * @param usernames Array scraped usernames
    */
   let scrapeUsernamesFromPhotoUrls = ( urls = [], usernames = []) => {
-    let url = 'https://www.instagram.com' + urls.shift();
+    // Setup request
+    let url = 'https://www.instagram.com' + urls.shift(),
+      request_args = {
+        url: url,
+        success: (result) => {
+          // Dig, dig, dig...
+          let jsonElement = '';
+          jsonElement = result.split('window._sharedData = ')[1];
+          jsonElement = jsonElement.split(';</script>')[0];
+          jsonElement = JSON.parse(jsonElement); // We've carved out a JSON object
 
-    request_args = {
-      url: url,
-      success: (result) => {
-        // Dig, dig, dig...
-        let jsonElement = '';
-        jsonElement = result.split('window._sharedData = ')[1];
-        jsonElement = jsonElement.split(';</script>')[0];
-        jsonElement = JSON.parse(jsonElement);
+          let username = jsonElement.entry_data.PostPage[0].graphql.shortcode_media.owner.username;
 
-        let username = jsonElement.entry_data.PostPage[0].graphql.shortcode_media.owner.username;
+          console.log('(%d/%d) Getting owner of: %s ... %s', usernames.length+1, (urls.length+usernames.length)+1, url, username);
+          usernames.push ( username );
 
-        console.log('(%d/%d) Getting owner of: %s ... %s', usernames.length, (urls.length+usernames.length), url, username);
-        usernames.push ( username );
+          if (urls.length) {
+            // console.log('usernames scraped: %d (%d to go)', usernames.length, urls.length);
+            setTimeout(() => { scrapeUsernamesFromPhotoUrls(urls, usernames); }, 900);
+          }
+          else {
+            // usernames = [].concat.apply([], usernames); // flatten array
+            console.log('Deduplicating username list from %d usernames', usernames.length);
+            usernames = dedupe([].concat(usernames)); // deduplicate
+            console.log('Deduplicated username list to %d usernames', usernames.length);
+            scrapeUserProfiles( usernames );
+          }
+        },
+        error: () => { return false; }
+      };
 
-        if (urls.length) {
-          // console.log('usernames scraped: %d (%d to go)', usernames.length, urls.length);
-          setTimeout(function() { scrapeUsernamesFromPhotoUrls(urls, usernames); }, 1000);
-        }
-      },
-      error: () => { return false; }
-    };
+    // Send request
+    $.ajax( request_args );
+  }
+
+
+  /**
+   * scrapeUserProfiles
+   *
+   * @param usernames Array Usernames to scrape
+   */
+  let scrapeUserProfiles = ( usernames = [], profiles = [] ) => {
+    // Setup request
+    let username = usernames.shift(),
+      url = "https://www.instagram.com/{{username}}/?__a=1".replace("{{username}}", username ),
+      request_args = {
+        url: url,
+        success: (result) => {
+          result = result.user;
+
+          let fullname = result.full_name,
+            fname = (fullname) ? (fullname.split(' ').length > 0) ? fullname.split(' ')[0] : '' : '',
+            lname = (fullname) ? (fullname.split(' ').length > 1) ? fullname.replace(fname, '').trim() : '' : '',
+            followers = (result.followed_by.count) ? result.followed_by.count : 0,
+            following = (result.follows.count) ? result.follows.count : 0,
+            profile_url1 = 'https://www.instagram.com/' + username,
+            // bio = (result.biography) ? '"' + result.biography.replace(/[^\x20-\x7E]/gmi, "") + '"' : '""', // strips emojis
+            bio = (result.biography) ? '"' + result.biography.replace(/(\r\n|\n|\r)/gm,"") + '"' : '""',
+            external_url = (result.external_url) ? result.external_url : '', // @TODO we shouldn't be callign two profile_urls
+            id = (result.id) ? result.id : 0,
+            profile = [username, fname, lname, '', followers, following, profile_url1, bio, external_url, id];
+
+          console.log('(%d/%d) Getting profile of: %s ... OK!', profiles.length+1, (usernames.length+profiles.length)+1, username);
+          console.log(bio);
+          profiles.push( profile );
+
+          if (usernames.length) {
+            setTimeout(() => { scrapeUserProfiles(usernames, profiles); }, 900);
+          }
+          else {
+            outputScrape( profiles );
+          }
+        },
+        error: () => { return false; },
+        dataType: 'json'
+      };
 
     $.ajax( request_args );
   }
 
+  /**
+   * outputScrape
+   *
+   * @param profiles Array of scraped profiles
+   */
+  let outputScrape = ( profiles = [] ) => {
+    let myWindow,
+      scrapeDate = new Date(),
+      scrapeData = "";
+
+    // Build output string
+    scrapeData += '<textarea style="width:1600px;height:900px;padding:10px;">';
+    profiles.map( (profile) => {
+      scrapeData += profile.join(",");
+      scrapeData += "\r";
+    });
+    scrapeData += '</textarea>';
+
+    // Open window
+    scrapeId = 'scrape' + scrapeDate.getTime();
+    myWindow = window.open( scrapeId, "_blank" );
+    myWindow.document.write( scrapeData );
+  }
 
   /**
    * getProfileByUser
@@ -112,13 +217,14 @@ jQuery(function() {
             lname = result.full_name.replace(fname, '').trim(),
             followers = result.followed_by.count,
             following = result.follows.count,
-            profile = 'https://instagram.com/' + username,
+            profile_url1 = 'https://instagram.com/' + username,
             bio = '"' + result.biography + '"',
-            profile_url = (result.external_url) ? result.external_url : '',
+            external_url = (result.external_url) ? result.external_url : '', // @TODO we shouldn't be callign two profile_urls
             id = result.id,
-            userArray = [username, fname, lname, '', followers, following, profile, bio, profile_url, id];
+            profile = [username, fname, lname, '', followers, following, profile_url1, bio, external_url, id];
 
-          return userArray;
+
+          return profile;
         },
         error: () => { return false; },
         dataType: 'json'
@@ -153,27 +259,43 @@ jQuery(function() {
 
 
   /**
-   * init
+   * Boolean functions
    *
-   * Bootstrap our scraper plugin
    */
-  let init = function() {
-
-    let cachebuster = new Date();
-    cachebuster = cachebuster.getTime();
-
-    // Inject styles
-    jQuery('<link href="//loc.insta.loc/prospector.css?'+ cachebuster +'" rel="stylesheet"></link>').appendTo('head');
-
-    // Inject dom elements
-    jQuery('<div href="#" id="ng_prospector"><a href="#" class="btn scrape">Scrape <span class="count"></span></a></div>').appendTo('body');
-
-    // Inject scripts
-    // jQuery('<script src="/loc.insta.loc/lib/clipboard.min.js"></script>').appendTo('body');
-
-    // Add Event Listeners
-    addListeners();
+  let is_profile = () => {
+    let test = getTitle();
+    return (test.length > 0);
   }
+  let is_photo = () => {
+    let test = getUsername();
+    return (test.length > 0);
+  }
+
+
+  /**
+   * Getters
+   *
+   */
+  let getTitle = () => {
+    return jQuery('section main article header h1').text();
+  }
+  let getUsername = () => {
+    return jQuery('article header div div div a').text();
+  }
+
+  /**
+   * dedupe
+   *
+   * De-duplicate items in array
+   *
+   * @param arr Array to dedupe
+   */
+  var dedupe = (arr) => {
+    return arr.filter((elem, pos, arr2) => {
+      return arr2.indexOf(elem) == pos;
+    });
+  }
+
 
   // All aboard!
   init();
